ad65f4fb274d936488bfeaeb2a2d5107ffd9ca10
jocs1989 <57869321+jocs1989@users.noreply.github.com>
2023-03-03 21:46:33 -0600

    Mejora

--- src/middleware/security/security.cors.js
+++ src/middleware/security/security.cors.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 export function managerCors(app) {
   app.use(
     cors({
-      origin: ["http://localhost:8080/"],
+      origin: ["http://localhost:8080/","https://ecommerce-production-d9ed.up.railway.app/"],
       methods: ["GET", "POST", "PUT", "DELETE"],
       allowedHeaders: ["Content-Type", "Authorization"],
       credentials: true,
